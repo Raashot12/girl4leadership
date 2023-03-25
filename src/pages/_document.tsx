a0ed0e7c044f55a7901db971ec678d9e069cfa25
Rasheed Iskilu <riskilu@plateaumed.com>
2023-03-25 10:19:13 +0100

    done

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,38 +16,23 @@ export default class AppDocument extends Document {
           <link rel="apple-touch-icon" href="/icon.png" />
           <meta name="theme-color" content="#fff" />
           <Script
-            id="GTM-NC7P28Z"
+            id="GTM-N"
             strategy="afterInteractive"
             dangerouslySetInnerHTML={{
               __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-NC7P28Z');`,
+        })(window,document,'script','dataLayer','GTM-MMH28TM');`,
             }}
           ></Script>
-
-          {/* <Script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=G-R8DGDBFYYY"
-          ></Script>
-          <Script id="3">
-            dangerouslySetInnerHTML=
-            {{
-              __html: `window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-            gtag('config', 'G-R8DGDBFYYY');`,
-            }}
-          </Script> */}
         </Head>
         <body>
           <Main />
           <NextScript />
           <noscript
             dangerouslySetInnerHTML={{
-              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NC7P28Z"
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MMH28TM"
             height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
             }}
           ></noscript>
@@ -56,3 +41,17 @@ export default class AppDocument extends Document {
     );
   }
 }
+//   {/* <Script
+//           async
+//           src="https://www.googletagmanager.com/gtag/js?id=G-R8DGDBFYYY"
+//         ></Script>
+//         <Script id="3">
+//           dangerouslySetInnerHTML=
+//           {{
+//             __html: `window.dataLayer = window.dataLayer || [];
+// function gtag(){dataLayer.push(arguments);}
+// gtag('js', new Date());
+
+//           gtag('config', 'G-R8DGDBFYYY');`,
+//           }}
+//         </Script> */}
