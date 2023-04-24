0b99014ddcd78c5d65018c4a5314de7411b70707
Rasheed Iskilu <riskilu@plateaumed.com>
2023-04-24 04:43:08 +0100

    done

--- src/pages/blogdetails/[slug].tsx
+++ src/pages/blogdetails/[slug].tsx
@@ -29,7 +29,9 @@ const BlogInformation = ({ frontmatter, markdown }) => {
 export default BlogInformation;
 
 export async function getStaticProps({ params: { slug } }) {
-  const fileContent = matter(fs.readFileSync(`./src/posts/${slug}.md`, 'utf8'));
+  const fileContent = matter(
+    fs.readFileSync(`./content/blogs/${slug}.md`, 'utf8')
+  );
   let frontmatter = fileContent.data;
   const markdown = fileContent.content;
 
@@ -38,7 +40,7 @@ export async function getStaticProps({ params: { slug } }) {
   };
 }
 export async function getStaticPaths() {
-  const filesInProjects = fs.readdirSync('./src/posts');
+  const filesInProjects = fs.readdirSync('./content/blogs');
 
   // Getting the filenames excluding .md extension
   // and returning an array containing slug (the filename) as params for every route
