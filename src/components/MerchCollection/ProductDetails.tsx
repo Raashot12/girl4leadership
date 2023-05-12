b2f4a8bba49eade1b0c6f5812efa017db775b16a
Rasheed Iskilu <riskilu@plateaumed.com>
2023-05-12 08:11:23 +0100

    done

--- src/components/MerchCollection/ProductDetails.tsx
+++ src/components/MerchCollection/ProductDetails.tsx
@@ -1,4 +1,5 @@
-import { Box, Container, Grid } from '@mantine/core';
+import { Carousel } from '@mantine/carousel';
+import { Box, Container, Grid, Rating } from '@mantine/core';
 import React from 'react';
 // import { createClient } from 'next-sanity';
 
@@ -7,8 +8,18 @@ const ProductDetails = () => {
     <Box mt={77}>
       <Container size={'xl'}>
         <Grid>
-          <Grid.Col md={6}></Grid.Col>
-          <Grid.Col md={6}></Grid.Col>
+          <Grid.Col md={6}>
+            <Carousel>
+              <Carousel.Slide>1</Carousel.Slide>
+              <Carousel.Slide>2</Carousel.Slide>
+              <Carousel.Slide>3</Carousel.Slide>
+              <Carousel.Slide>4</Carousel.Slide>
+              <Carousel.Slide>5</Carousel.Slide>
+            </Carousel>
+          </Grid.Col>
+          <Grid.Col md={6}>
+            <Rating value={2} />
+          </Grid.Col>
         </Grid>
       </Container>
     </Box>
