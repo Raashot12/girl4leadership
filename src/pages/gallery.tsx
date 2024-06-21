76f0113d74a2df15b58c5483cf68ca12730fb4df
Rasheed Iskilu <riskilu@plateaumed.com>
2024-06-21 06:51:14 +0100

    WIP

--- src/pages/gallery.tsx
+++ src/pages/gallery.tsx
@@ -7,26 +7,14 @@ import { GalleryDataTypes } from 'types/merchSection';
 type GalleryProps = {
   galleryData: GalleryDataTypes[];
 };
-const GalleryPage: React.FC<GalleryProps> = ({ galleryData }) => {
+const GalleryPage: React.FC<GalleryProps> = () => {
   return (
     <Layout pageTitle="Gallery">
-      <Galleries
-        galleryData={
-          galleryData as unknown as {
-            id: number;
-            imageURL: string;
-          }[]
-        }
-      />
+      <Galleries />
     </Layout>
   );
 };
 export const getStaticProps: GetStaticProps<GalleryProps> = async () => {
-  // const res = await fetch(
-  //   `${process.env.NEXT_PUBLIC_APP_API_SERVICE_BASE_URL}/api/gallery`
-  // );
-  // const value = await res.json();
-
   return {
     props: {
       galleryData: [] as GalleryDataTypes[],
